83cc5d9b4ab226319756e630d4f1c548b07a39ca
Enrico Cordero <e.cordero.2645@vallauri.edu>
2024-06-02 11:23:26 +0200

    cities added, started detailing hotels from the city selected

--- index.js
+++ index.js
@@ -38,15 +38,28 @@ $(document).ready(function(){
 	rq.then(function(response){
 		response.data.forEach(item => {
 			let divCitta = $("<div>").addClass("form-check form-check-inline").appendTo(sezCitta)
-			/*$("<input>").prop({
-				"type": "radio",
-				"name": "inlineRadioOptions",
-				"id": "inlineRadio" + item["id"],
-				"value": "option" + item["id"]
-			})*/
-			$("<div>").text(item["citta"])
-			.appendTo(divCitta)
-			.on("click", function(){
+			let div = $("<div>").css({
+				"width": "200px",
+				"height": "200px",
+				"background-image": "url(img/citta/" + item["citta"] + ".jpg)",
+				"background-size": "cover",
+				"margin": "10px",
+				"border-radius": "10px",
+				"background-repeat": "no-repeat",
+				"padding": "20px",
+				"color": "white",
+				"box-shadow": "4px 4px green, 8px 8px white, 12px 12px red",
+				"transition": "all 200ms"
+			}).appendTo(divCitta)
+			div.append($("<span>").text(item["citta"] + " "))	
+			div.append($("<img>").prop("src", "img/italianFlag.png"))
+			div.on("mouseenter", function(){
+				$(this).css("scale", "1.05")
+			})
+			div.on("mouseout", function(){
+				$(this).css("scale", "1.00")
+			})
+			div.on("click", function(){
 				sezHotel.empty()
 				vetCitta = []
 				let citta = item["citta"]
@@ -65,11 +78,15 @@ $(document).ready(function(){
 						let h4 = $("<h4>").appendTo(divInterno)
 						$("<span>").text(hotel["nomeHotel"]).appendTo(h4)
 						for(let i = 0; i < hotel["stelle"]; i++){
-							$("<img>").prop("src", "img/star.png").appendTo(h4)
+							$("<img>").prop("src", "img/star.png").css("margin", "1px").appendTo(h4)
 						}
 						$("<p>").text(hotel["descrizione"]).appendTo(divInterno)
-						$("<a>").prop("href", "#").addClass("btn btn-primary").text("Dettagli")
+						$("<a>").prop("href", "#").css("margin", "5px").addClass("btn btn-primary").text("Dettagli")
 						.on("click", function(){ vediDettagli(hotel) }).appendTo(divInterno)
+						$("<a>").prop("href", "#").css("margin", "5px").addClass("btn btn-primary").text("Recensioni")
+						.on("click", function(){ alert("recensioni") }).appendTo(divInterno)
+						$("<a>").prop("href", "#").css("margin", "5px").addClass("btn btn-primary").text("Location")
+						.on("click", function(){ alert("Geolocation") }).appendTo(divInterno)
 						$("<p>Prezzi a partire da </p>").append($("<span>").text("80€").addClass("prezzo")).appendTo(divInterno)
 					});
 				})
